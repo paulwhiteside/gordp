94ccc3c7cc03ca293c4ec1340d4d494faf12ea7f
paulwhiteside <paul.whiteside@globalscaledigital.com>
2022-11-19 18:40:53 +0000

    Added unit testing for expression parser.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"gordp/functions"
 	"gordp/rdp"
-	"gordp/spreadsheet"
 )
 
 func Go() int {
@@ -13,8 +12,9 @@ func Go() int {
 
 func main() {
 	//s := `3.14 + 9 / (-3 ^ 2) + 1`
-	s := `=1 + 2 + 3 + 4 + 5 + 6  + sum(1,2-1.53663763,3.14) + 500`
+	s := `1 + 2 + 3 + 4 + 5 + 6  + sum(1,2-1.53663763,3.14) + 500`
 	//s := `1+3 * 2-8 + 7 * 3 * 4+4.44`
+	//s := `-2 + 1`
 
 	ast := rdp.BuildAst(s)
 	fmt.Println(ast)
@@ -31,28 +31,29 @@ func main() {
 	fmt.Println(s, "=", result)
 
 	// TODO Move this round tripping to a proper unit test
-	fmt.Println("---------------------------------------")
-	n := 35
-	for idx := 1; idx < n; idx++ {
-		a := spreadsheet.ToBase26(idx)
-		b := spreadsheet.FromBase26(a)
-		msg := fmt.Sprintf("%2d %2s %2d", idx, a, b)
-		fmt.Println(">>", msg)
-	}
+	/*
+		fmt.Println("---------------------------------------")
+		n := 35
+		for idx := 1; idx < n; idx++ {
+			a := spreadsheet.ToBase26(idx)
+			b := spreadsheet.FromBase26(a)
+			msg := fmt.Sprintf("%2d %2s %2d", idx, a, b)
+			fmt.Println(">>", msg)
+		}
 
-	mybook := spreadsheet.NewBook()
-	mybook.AddSheet("Sheet 1", 10, 10)
-	mybook.AddSheet("Sheet 2", 10, 10)
-	mybook.AddSheet("Sheet 3", 10, 10)
+		mybook := spreadsheet.NewBook()
+		mybook.AddSheet("Sheet 1", 10, 10)
+		mybook.AddSheet("Sheet 2", 10, 10)
+		mybook.AddSheet("Sheet 3", 10, 10)
 
-	sheet2 := mybook.GetSheet("Sheet 2")
+		sheet2 := mybook.GetSheet("Sheet 2")
 
-	sheet2.AddCell("A6", 3.14, "")
-	fmt.Println("**>", sheet2.GetCell("A6"))
-	cell := sheet2.GetCell("A6")
+		sheet2.AddCell("A6", 3.14, "")
+		fmt.Println("**>", sheet2.GetCell("A6"))
+		cell := sheet2.GetCell("A6")
 
-	cell.SetFormula("=2 * 7")
-	cell.Calculate()
-	fmt.Println("**>", sheet2.GetCell("A6"))
-
+		cell.SetFormula("=2 * 7")
+		cell.Calculate()
+		fmt.Println("**>", sheet2.GetCell("A6"))
+	*/
 }
---
+++ main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"gordp/functions"
+	"gordp/rdp"
+	"testing"
+)
+
+func TestCalulate(t *testing.T) {
+	s := `=1 + 2 + 3 + 4 + 5 + 6  + sum(1,2-1.53663763,3.14) + 500`
+	ast := rdp.BuildAst(s)
+	interpreter := rdp.NewIntrepreter()
+
+	//Register functions with the interpreter
+	interpreter.RegisterFunction("sum", functions.Sum)
+	result := interpreter.Eval(ast)
+
+	if result != 525.60336237 {
+		t.Error("Error")
+	}
+}
+
+func TestTableCalculate(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected int
+	}{
+		{`2+1`, 3},
+		{`-2+1`, -1},
+	}
+
+	for _, test := range tests {
+		ast := rdp.BuildAst(test.input)
+		interpreter := rdp.NewIntrepreter()
+
+		result := interpreter.Eval(ast)
+
+		if result == test.expected {
+			t.Error("input: {} expected:{} result:{}", test.input, test.expected, result)
+		}
+
+	}
+}

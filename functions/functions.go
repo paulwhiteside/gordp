d866e7e59d4a4e2003f071240e87210dfca5b091
paulwhiteside <paul.whiteside@globalscaledigital.com>
2023-01-16 21:09:39 +0000

    Simplified Sum function

--- functions/functions.go
+++ functions/functions.go
@@ -1,6 +1,38 @@
 package functions
 
+func addValues(v1 interface{}, v2 interface{}) interface{} {
+	var result interface{}
+	switch v1.(type) {
+	case int:
+		switch v2.(type) {
+		case int:
+			result = v1.(int) + v2.(int)
+		case float64:
+			result = float64(v1.(int)) + v2.(float64)
+		}
+	case float64:
+		switch v2.(type) {
+		case int:
+			result = v1.(float64) + float64(v2.(int))
+		case float64:
+			result = v1.(float64) + v2.(float64)
+		}
+	}
+	return result
+}
+
 func Sum(arg ...interface{}) interface{} {
+
+	var tot interface{}
+	tot = 0
+	for i := range arg {
+		tot = addValues(tot, arg[i])
+	}
+
+	return tot
+}
+
+func SSum(arg ...interface{}) interface{} {
 
 	var tot float64
 	for i := range arg {
